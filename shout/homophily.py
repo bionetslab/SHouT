a5fbf218ece86f87765e61b79ab7817b0f656e96
David B. Blumenthal <david.b.blumenthal@fau.de>
2024-01-29 16:48:11 +0100

    some more code

--- shout/homophily.py
+++ shout/homophily.py
@@ -1,29 +1,45 @@
+from .utility import retrieve_extended_neighborhoods
+import numpy as np
 
-def global_homophily(adata):
+def global_homophily(adata, adj_matrix, adj_matrix_homophilic, copy=False):
     """
 
     Parameters
     ----------
     adata :
+    adj_matrix :
+    adj_matrix_homophilic :
 
     Returns
     -------
 
     """
-    pass
+    if copy:
+        return adj_matrix_homophilic.sum() / adj_matrix.sum()
+    adata.uns['global_homophily'] = adj_matrix_homophilic.sum() / adj_matrix.sum()
 
 
-def local_homophily(adata, radius):
+def local_homophily(adata, shortest_path_distances, radius, adj_matrix, adj_matrix_homophilic, copy=False):
     """
 
     Parameters
     ----------
     adata :
-    cluster_key :
+    shortest_path_distances :
     radius :
+    adj_matrix :
+    adj_matrix_homophilic :
 
     Returns
     -------
 
     """
-    pass
+    extended_neighborhoods = retrieve_extended_neighborhoods(shortest_path_distances, radius)
+    local_homophilies = np.zeros(len(extended_neighborhoods))
+    for cell, extended_neighborhood in extended_neighborhoods.items():
+        sub_adj_matrix = adj_matrix[np.ix_([extended_neighborhood], [extended_neighborhood])]
+        sub_adj_matrix_homophilic = adj_matrix_homophilic[np.ix_([extended_neighborhood], [extended_neighborhood])]
+        local_homophilies[cell] = sub_adj_matrix_homophilic.sum() / sub_adj_matrix.sum()
+    if copy:
+       return local_homophilies
+    adata.obsm['local_homophily'] = local_homophilies
--- shout/utility.py
+++ shout/utility.py
@@ -1,3 +1,7 @@
+from squidpy.gr import spatial_neighbors
+from scipy.sparse.csgraph import shortest_path
+from scipy.sparse import csr_matrix
+
 
 def compute_spatial_graph(adata, cluster_key, coord_type='generic'):
     """Computes spatial graph using Delaunay triangulation and annotates edges as homophilic or heterophilic.
@@ -12,10 +16,17 @@ def compute_spatial_graph(adata, cluster_key, coord_type='generic'):
     -------
 
     """
-    pass
+    adj_matrix, _ = spatial_neighbors(adata, delaunay=True, coord_type=coord_type, copy = True)
+    shortest_path_distances = shortest_path(adj_matrix)
+    adj_matrix_homophilic = csr_matrix(adj_matrix)
+    support_adj_matrix = adj_matrix.nonzero()
+    for edge in zip(support_adj_matrix[0], support_adj_matrix[1]):
+        if adata.obs[cluster_key].iloc[edge[0]] != adata.obs[cluster_key].iloc[edge[1]]:
+            adj_matrix_homophilic[edge] = 0
+    return adj_matrix, adj_matrix_homophilic, shortest_path_distances
 
 
-def retrieve_extended_neighborhood(adata, cell, radius):
+def retrieve_extended_neighborhoods(shortest_path_distances, radius):
     """
 
     Parameters
@@ -28,4 +39,9 @@ def retrieve_extended_neighborhood(adata, cell, radius):
     -------
 
     """
-    pass
+    all_close_cell_pairs = (shortest_path_distances <= radius).nonzero()
+    all_close_cell_pairs = list(zip(all_close_cell_pairs[0], all_close_cell_pairs[1]))
+    extended_neighborhoods = {cell: [] for cell in range(shortest_path_distances.shape[0])}
+    for close_cell_pair in all_close_cell_pairs:
+        extended_neighborhoods[close_cell_pair[0]].append(close_cell_pair[1])
+    return extended_neighborhoods
